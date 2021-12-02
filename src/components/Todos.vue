6b9728795773eb9ecd72413efad30d4e2745ef26
LorenzoChukwuebuka <lawrenceobi2@gmail.com>
2021-12-02 03:34:57 -0800

    final-updates

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,19 @@
 <template>
   <main>
     <div>
+      <div class="legend">
+        <span> Double click to mark as complete</span>
+        <span> <span class="incomplete-box"></span> = Incomplete </span>
+        <span> <span class="complete-box"></span> = Complete </span>
+      </div>
       <div class="todos">
-        <div class="todo" v-for="todo in allTodos" :key="todo.id">
+        <div
+          @dblclick="onDblclick(todo)"
+          class="todo"
+          v-for="todo in allTodos"
+          :key="todo.id"
+          :class="{'is-complete':todo.completed}"
+        >
           {{ todo.title }}
           <small>
             <i class="fa fa-trash" @click="deleteTodo(todo.id)"> </i>
@@ -24,7 +35,16 @@ export default {
   },
 
   methods: {
-    ...mapActions(['fetchTodos', 'deleteTodo'])
+    ...mapActions(['fetchTodos', 'deleteTodo', 'updateTodo']),
+    onDblclick (todo) {
+      const upTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed
+      }
+
+      this.updateTodo(upTodo)
+    }
   }
 }
 </script>
@@ -50,5 +70,33 @@ small {
   right: 10px;
   color: white;
   cursor: pointer;
+}
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #48834d;
+}
+
+.is-complete{
+  background:#35495e;
+  color:#fff;
+}
+
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
